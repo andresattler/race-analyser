8d82a5b7f67323d29c52d2deb51f8d2d69624d41
andresattler <andresattler.webdev@gmail.com>
2017-07-30 10:03:58 +1200

    simplified by using async await

--- src/server/routes/general.js
+++ src/server/routes/general.js
@@ -1,69 +1,30 @@
 import mongoose from 'mongoose'
 
-import { Race } from '../model'
+import { Race, Driver } from '../model'
 
 mongoose.Promise = global.Promise
 
-const count = key => val => Race.count({ [key]: val }).exec()
+const count = key => val => Race.count({ [key]: val })
 
-/*
-Race.find({}, { track_id: 1 }).exec()x
-  .then((data) => {
-    console.log(data)
-  })
-  .catch((err) => {
-    // eslint-disable-next-line no-console
-    console.log(err)
-  })
-*/
-const getNumberOfDrivers = new Promise((resolve) => {
-  let drivers
-  Race.find().distinct('challenger').exec()
-    .then((data) => {
-      drivers = data
-    })
-    .then(() => Race.find().distinct('opponent').exec())
-    .then((data) => {
-      drivers.concat(data)
-      drivers.sort((a, b) => a - b).filter((a, b) => a !== b)
-      resolve(drivers.length)
-    })
-})
-
-const general = (req, res) => {
+const general = async (req, res) => {
   const countStatus = count('status')
   const data = {}
-  Promise.all([
+  data.status = await Promise.all([
     countStatus('finished'),
     countStatus('declined'),
     countStatus('retired'),
-  ]).then((values) => {
-    data.status = values
-  }).then(() => {
-    const countWeather = count('weather')
-    return Promise.all([
-      countWeather('sunny'),
-      countWeather('rainy'),
-      countWeather('snowy'),
-      countWeather('thundery'),
-    ])
-  })
-  .then((values) => {
-    data.weather = values
-  })
-  .then(() => getNumberOfDrivers)
-  .then((numberOfDrivers) => {
-    data.numberOfDrivers = numberOfDrivers
-    return Race.count().exec()
-  })
-  .then((numberOfRaces) => {
-    data.numberOfRaces = numberOfRaces
-    return Race.distinct('track_id').exec()
-  })
-  .then((tracks) => {
-    data.numberOfTracks = tracks.length
-    res.send(data)
-  })
+  ])
+  const countWeather = count('weather')
+  data.weather = await Promise.all([
+    countWeather('sunny'),
+    countWeather('rainy'),
+    countWeather('snowy'),
+    countWeather('thundery'),
+  ])
+  data.numberOfDrivers = await Driver.count()
+  data.numberOfRaces = await Race.count()
+  data.numberOfTracks = await Race.distinct('track_id').count()
+  res.send(data)
 }
 
 export default general
